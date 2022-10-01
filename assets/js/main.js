19808acaa81aa0c86ab4931382badfcd0eafcf5d
Catalina <catalina.castillo19c@gmail.com>
2022-10-01 12:08:04 -0300

    avance2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ dropDowns.forEach((dropDown) => {
     const menu = dropDown.querySelector(".menu");
     const menuItems = dropDown.querySelectorAll(".menu li");
     const selectTitle = dropDown.querySelector(".select-title");
-
     select.addEventListener("click", () => {
         select.classList.toggle("select-clicked");
         arrow.classList.toggle("arrow-rotate");
@@ -29,3 +28,95 @@ dropDowns.forEach((dropDown) => {
         });
     });
 });
+
+
+
+
+
+function poto() {
+    const destinos = document.getElementById("mySelect");
+    const ciudades = ['San Pedro de Atacama', 'Isla de Pascua', 'La Serena, playas y astronmía', 'Distrito de Lagos y Volcanes', 'Valparaíso, Santiago y Viñedos', 'Patagonia Norte y Bosque Nativo'];
+    ciudades.forEach((a, i) => {
+        const imagen = document.createElement("div");
+        imagen.setAttribute("class", "container_destinos_col");
+        imagen.innerHTML = ` 
+    
+       
+          <div
+            class="container_destinos_img-big"
+            style="
+              background-image: url(./assets/img/destinos/${i}.jpg);
+            "
+          >
+            <div class="container_destinos_info">
+              <div class="dropdown">
+                <button
+                  onclick="myFunction()"
+                  class="container_destinos_info-a"
+                >
+                  ${a}
+                </button>
+
+                <div id="myDropdown" class="dropdown-content">
+                  <a
+                    class="dropdown-item"
+                    href="#"
+                    >Tours</a
+                  >
+                  <a
+                    class="dropdown-item"
+                    href="#
+                    >Actividades</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        
+    
+      `;
+
+        destinos.appendChild(imagen);
+
+    })
+}
+
+poto();
+
+
+function formulario() {
+    const destinosForm = document.getElementById("myForm");
+    const ciudadesForm = ['San Pedro de Atacama', 'Isla de Pascua', 'La Serena, playas y astronmía', 'Distrito de Lagos y Volcanes', 'Valparaíso, Santiago y Viñedos', 'Patagonia Norte y Bosque Nativo'];
+    ciudadesForm.forEach((a, i) => {
+        const formulario = document.createElement("option");
+        destinosForm.setAttribute("class", "sel")
+        formulario.setAttribute("class", "paraAbajo");
+        formulario.text = a;
+       
+
+        destinosForm.appendChild(formulario);
+
+    })
+}
+
+formulario();
+
+
+function date() {
+    const destinosForm = document.getElementById("mydate");
+    const ciudadesForm = ['San Pedro de Atacama', 'Isla de Pascua', 'La Serena, playas y astronmía', 'Distrito de Lagos y Volcanes', 'Valparaíso, Santiago y Viñedos', 'Patagonia Norte y Bosque Nativo'];
+    ciudadesForm.forEach((a, i) => {
+        const formulario = document.createElement("date");
+        destinosForm.setAttribute("class", "sel")
+        formulario.setAttribute("class", "paraAbajo");
+        formulario.text = a;
+       
+
+        destinosForm.appendChild(formulario);
+
+    })
+}
+
+date();
+
+
